fe6d4260a883fd3111debc5d0fb0b38bb3e4877f
Christopher Werther <wildcatwerther@gmail.com>
2019-09-04 15:56:08 -0700

    Update guess functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z],
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 var guessedLetters = [];
 
@@ -12,3 +12,20 @@ var losses = 0;
 var updateLettersToGuess = function(){
     lettersToGuess = letters[Math.floor(Math.random() * letters.length)];
 }
+
+var updateRemainingGuesses = function() {
+    document.querySelector("#remaining-guesses").innerHTML = remainingGuesses;
+}
+
+var updateCorrectGuesses = function(){
+    document.querySelector("#correct-guesses").innerHTML = remainingGuesses.join(",");
+}
+
+var reset = function(){
+    remainingGuesses = 9;
+    guessedLetters = [];
+    updateLettersToGuess();
+    updateRemainingGuesses();
+    updateCorrectGuesses();
+};
+
